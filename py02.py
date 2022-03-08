17210a33a97c4c662f9d45d8274b7cb025b2b7f1
Yul <80104121+ikiki1008@users.noreply.github.com>
2022-03-08 15:33:16 +0900

    Add files via upload

---
+++ py02.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.shape('turtle')
+>>> turtle.color('green')
+>>> turtle.forward(200)
+>>> turtle.right(100)
+>>> turtle.right(10)
+>>> turtle.forward(100)
+>>> turtle.forward(100)
+>>> turtle.left(20)
+>>> turtle.left(30)
+>>> turtle.forward(200)
+>>> turtle.right(110)
+>>> turtle.forward(200)
+>>> turtle.color('red')
+>>> turtle.right(90)
+>>> turtle.forward(200)
+>>> turtle.forward(200)
+>>> #터틀ㅌ재미없다ㅋㅋㅋㅋㅋㅋㅋ
