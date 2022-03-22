27d0eedece7766543a3d3fa0f618d81babb43be0
Yul <80104121+ikiki1008@users.noreply.github.com>
2022-03-22 15:32:54 +0900

    Add files via upload

---
+++ 서식출력및진수.py
@@ -0,0 +1,19 @@
+#파이썬 서식출력 practice
+
+
+print('i love {} for "{}"'.format('python','java'))
+print('{0} and {1}'.format('python','java'))
+print('{1} and {0}'.format('python','java'))
+
+print('\t tab \t practice')
+print('welcome to Python world\n i just wanna sleep \n ha view ever seen my shoes?') #\n is for new line
+print('this slash used for \"important\" things')
+print('this also used for \'important\' things')
+print("\\\\\\ this shows 3 revers slashes, you must use 6 slashes")
+print('r"\n \t \" \\') #just run this on Python, dunno how to explain bruh
+
+#파이썬 진법바꾸기
+print(bin(11)) #2진수
+print(hex(11)) #16진수
+print(oct(11)) #8진수
+
