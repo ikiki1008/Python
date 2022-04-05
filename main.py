5695cbbc43af8579359ca8145f0b6ccf9f2b1d24
Yul <80104121+ikiki1008@users.noreply.github.com>
2022-04-05 14:32:13 +0900

    Add files via upload
    파이썬 별 출력하기

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
-#xy좌표 상에서 마음대로 거북이 움직이기
-import turtle
-import turtle as t
-import random
+#ㅍㅏ이썬 별 출력하기
 
-#폭과 높이, 펜 두께, exitCount는 윈도우창 밖으로 빠져나간 횟수를 세는것, angle과 dist는 임의로 이동할 거리와 각도
-#curX,Y는 현재 거북이의 위치를 저장하는 변수다
-swidth, sheight, pSize, exitCount = 300,300,3,0
-r,g,b, angle, dist, curX, curY = [0] * 7
+#.1
+for i in range(5):  #0부터 4까지 반복, 세로방향
+    for j in range(5): #0부터 4까지 반복, 가로방향
+        if j <= i: #세로방향 변수 i 만큼
+            print('*', end='') #별출력, end에 ''를 지정하여 줄바꿈을 하지 않는다
+    print() #가로방향으로 별을 다 그린뒤 다음줄로 넘어간다
 
-t.shape('turtle')
-t.pensize(pSize)
-t.setup(width = swidth + 30, height = sheight+30)
-t.screensize(swidth,sheight)
 
-while True:
-    r=random.random()
-    g=random.random()
-    b=random.random()
-    t.pencolor(r,g,b)
-    #임의의 색상 지정하고 각도는 0~360 범위에서, 거리는 1~100 범위에서 임의로 추출한다
+#.2
+for i in range(5):
+    for j in range(5):
+        if j==i: #세로방향 변수값이 서로 같을 시.
+            print('*', end='')
+    print()
 
-    angle = random.randrange(0,360)
-    dist = random.randrange(1,100)
-    t.left(angle)
-    t.forward(dist)
-    curX = turtle.xcor()
-    curY = turtle.ycor()
 
-    #pass 실행해서 if문을 그냥 종료하고 다시 while 문 수행한다.
-    #이범위를 벗어나면 else 부분을 실행한다.
-    if(-swidth /2 <= curX and curX <= swidth /2) and (-sheight /2 <= curY and curY <= sheight /2) :
-        pass
-    else:
-        t.penup()
-        t.goto(0,0)
-        t.pendown()
 
-        #거북이가 5회이상 넘어갈 경우 break 걸기
-        exitCount += 1
-        if exitCount >= 5 :
-            break
-t.done()
+#.3
+for i in range(5):
+    for j in range(5):
+        if j==i:
+            print('*', end='')
+        else:  #세로방향 변수값이 다를때는 공백을 출력해서 나오게 하라.
+            print(' ', end='')
+    print()
+
+
+#.4
+for i in range(5,0,-1): #5개부터 하나씩 줄어들게 하라.
+    for j in range(i):
+        print('*', end='')
+    print('')
+
+
+
+#.5
+for i in range(1,6):
+    for j in range(5-i):
+        print(' ', end='')
+    for j in range(1,i*2,1): #*2배씩 출력되게 하라.
+        print('*', end='')
+    print('')
+
+
+
+#.6
+for i in range(1,6):
+    for j in range(5-i):
+        print(' ', end='')
+    for j in range(1,2*i,1):
+        print('*',end='')
+    print('')
+for i in range(5):
+    for j in range(i):
+        print(' ',end='')
+    for j in range(10,1+i*2,-1):
+        print('*', end='')
+    print('')
