ffc0d24d3c0967d9d7a0d4409a0eff16bcf34cd4
Yul <80104121+ikiki1008@users.noreply.github.com>
2022-04-18 11:37:08 +0900

    Add files via upload
    시험치기시러

--- main.py
+++ main.py
@@ -1,60 +1,77 @@
-#ㅍㅏ이썬 별 출력하기
+# list tuple practice
+# slice example
+# for example when you want to bring 0 to 50, you should slice numbers as [0:4]
 
-#.1
-for i in range(5):  #0부터 4까지 반복, 세로방향
-    for j in range(5): #0부터 4까지 반복, 가로방향
-        if j <= i: #세로방향 변수 i 만큼
-            print('*', end='') #별출력, end에 ''를 지정하여 줄바꿈을 하지 않는다
-    print() #가로방향으로 별을 다 그린뒤 다음줄로 넘어간다
+# from 0 to 30
+a = [0,10,20,30,40,50,60,70]
+a[0:4]
 
-
-#.2
-for i in range(5):
-    for j in range(5):
-        if j==i: #세로방향 변수값이 서로 같을 시.
-            print('*', end='')
-    print()
+# from 10 to 40
+a = [0,10,20,30,40,50,60,70]
+a[1:5]
 
 
+# from 20 to 50, add 20 numbers
+a = [0,10,20,30,40,50,60,70]
+a[2:6:2]
+# result = 20,40
 
-#.3
-for i in range(5):
-    for j in range(5):
-        if j==i:
-            print('*', end='')
-        else:  #세로방향 변수값이 다를때는 공백을 출력해서 나오게 하라.
-            print(' ', end='')
-    print()
+# pick up 40 to 60
+a = [0,10,20,30,40,50,60,70]
+a[4:7]
 
-
-#.4
-for i in range(5,0,-1): #5개부터 하나씩 줄어들게 하라.
-    for j in range(i):
-        print('*', end='')
-    print('')
+# pick up numbers from beginning to 50
+# let the first place blank, so they could bring numbers from beginning
+a = [0,10,20,30,40,50,60,70]
+a[:6]
+# result = 0,10,20,30,40,50
 
 
+a = [0,10,20,30,40,50,60,70]
+a[5:]
+# result = 50,60,70
 
-#.5
-for i in range(1,6):
-    for j in range(5-i):
-        print(' ', end='')
-    for j in range(1,i*2,1): #*2배씩 출력되게 하라.
-        print('*', end='')
-    print('')
+# sequence object
+# if you dont type any numbers in space. they show you the whole numbers in object
+a = [0,10,20,30,40,50,60,70]
+a[:]
+# or a[::]
+# result = 0,10,20,30,40,50,60,70
+
+
+# if you wanna bring numbers from beginning but until specific number, and wanna set rules
+a = [0,10,20,30,40,50,60,70]
+a[:7:3]
+# result = 0,30,60
+
+
+#특정 숫자부터 증폭시켜서 끝까지 나오게 하고 싶을때, 다른 방법으로는 [4:-1:3] 이 있지만 따옴표를 두번써서 간단히 출력
+a = [0,10,20,30,40,50,60,70]
+a[4::3]
+# result = 40,70
+
+# from beginning to the end, adds 2
+a[::2]
+# result = 0,20,40,60
+
+a[::-1]
+# result = 70,60,50,40,30,20,10,0
+
+# 6부터 거꾸로 2씩 줄여서 표현
+a[6:0:-2]
+# result = 60,40,20,0
 
 
 
-#.6
-for i in range(1,6):
-    for j in range(5-i):
-        print(' ', end='')
-    for j in range(1,2*i,1):
-        print('*',end='')
-    print('')
-for i in range(5):
-    for j in range(i):
-        print(' ',end='')
-    for j in range(10,1+i*2,-1):
-        print('*', end='')
-    print('')
+
+
+
+
+
+
+
+
+
+
+
+
